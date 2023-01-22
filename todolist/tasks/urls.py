a900660619f85ebaee21cc5f135521e555965db9
DavidSaruni <sarunidavid11126@gmail.com>
2023-01-23 02:11:11 +0300

    added css styling

--- todolist/tasks/urls.py
+++ todolist/tasks/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('', TaskList.as_view(), name="tasks"),
     path('task/<int:pk>/', TaskDetail.as_view(), name="task"),
     path('task_create', TaskCreate.as_view(), name="task_create"),
-    path('task_update/<int:pk>/', TaskUpdate.as_view(), name="task_update"),
+    path('task_utask_updatepdate/<int:pk>/', TaskUpdate.as_view(), name="task_update"),
     path('task_delete/<int:pk>/', TaskDelete.as_view(), name="task_delete"),
 ]
